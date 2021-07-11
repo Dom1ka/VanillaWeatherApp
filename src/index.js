339df113c3193136574c9ec59d3244383fc84ab7
Dom1ka <dom.soch95@gmail.com>
2021-07-11 17:09:26 +0100

    Added weather icon

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ function displayTemperarure(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -37,6 +38,11 @@ function displayTemperarure(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "708c6e03dd68365a43c77d48ed40d262";
